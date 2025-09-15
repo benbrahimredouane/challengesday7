b4bf8b20190c99865cff6b37aa04b2092fec702b
Redouane <benbrahimredouane664.com>
2025-09-15 18:39:46 +0100

    push challenge (Structure et Pointeur de Structure)

---
+++ challenge7/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef struct {
+    int jour;
+    int mois;
+    int annee;
+}Date;
+
+int main(){
+    Date *p;
+    Date date;
+    p = &date ;
+    printf("saisir la date complet dd/mm/ans \n");
+    
+    scanf("%d",&p->jour);
+    
+    scanf("%d",&p->mois);
+    
+    scanf("%d",&p->annee);
+     
+
+    printf("\t ===le jour est : %d",p->jour);
+    printf("\t ===le mois est : %d", p->mois);
+    printf("\t ===l annee est : %d",p->annee);
+
+return 0;
+}
